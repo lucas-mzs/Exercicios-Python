47f9a1dc65a080a3378b4841f3b3a02691ce94cc
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-01 19:21:46 -0300

    adding exercises
    Adicionando do exercícios 01 ao 05.

---
+++ exercicios/ex_02.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia o nome de uma pessoas e mostre uma mensagem de boas vindas.
+
+nome = input('Como você se chama? ')
+
+print(f'Boa tarde {nome}!')
---
+++ exercicios/ex_03.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro número: ')
+
+numero1 = float(numero1)
+numero2 = float(numero2)
+
+divisao_de_numeros = numero1 / numero2
+
+print(f'A divisão de {numero1} por {numero2} é {divisao_de_numeros}')
