a270a651d85469084a157beedd685cfd3d06e0ca
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-02 00:57:00 -0300

    adjusting the exercises
    ajustando alguns detalhes dos exercícios.

--- exercicios/ex_10.py
+++ exercicios/ex_10.py
@@ -1,1 +1,2 @@
-# Crie um programa que leia quanto dinheiro uma pessoas tem na carteira e mostre quantos dólares ela pode comprar. Considere U$$1,00 = R$3,37.
+# Crie um programa que leia quanto dinheiro uma pessoas tem na carteira e mostre quantos dólares ela pode comprar. 
+# Considere U$$1,00 = R$3,37.
