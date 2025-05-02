0ee8dc1a62a664c11975c15ed827534cbf2da0b9
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-01 21:10:07 -0300

    adding exercises
    adicionando novos exercícios.
    do exercício 16 ao 25.

---
+++ exercicios/ex_23.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# (a) O nome com todas as letras maiúsculas e minúsculas.
+# (b) Quantas letras ao todo (sem considerar espaços).
+# (c) Quantas letras tem o primeiro nome.
