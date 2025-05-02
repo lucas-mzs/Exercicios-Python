322c54dfc41ce1570a606c66d0e458d5ae53e78c
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-02 12:57:30 -0300

    performing the exercises
    realizando os exercícios 08 e 10.

--- exercicios/ex_08.py
+++ exercicios/ex_08.py
@@ -1,1 +1,13 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+metros = input(
+    'Quantos metros você quer converter para centímetros e milímetros: ')
+
+metros = float(metros)
+
+centimetros = metros * 100
+milimetros = metros * 1000
+
+resultado = f'{metros} convertido para centímetros é {centimetros} e convertido para milímetros é {milimetros}'
+
+print(resultado.replace('.', ','))
--- exercicios/ex_10.py
+++ exercicios/ex_10.py
@@ -1,2 +1,11 @@
 # Crie um programa que leia quanto dinheiro uma pessoas tem na carteira e mostre quantos dólares ela pode comprar. 
 # Considere U$$1,00 = R$3,37.
+
+valor_real = input('Quantos reais você tem? ')
+valor_real = float(valor_real)
+
+dolar = valor_real / 3.37
+
+resultado = f'Com R${valor_real} você pode comprar U${dolar:.2f} dolares'
+
+print(resultado)
