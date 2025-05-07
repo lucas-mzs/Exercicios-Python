45856b2da52aa25281251b657308f389d647f2f6
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-07 14:55:19 -0300

    performing exercises
    realizando os exercícios 18 e 19.

--- exercicios/ex_18.py
+++ exercicios/ex_18.py
@@ -1,1 +1,14 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+import math
+
+angulo_graus = float(input('Digite um ângulo qualquer: '))
+angulo_radianos = math.radians(angulo_graus)
+
+seno = math.sin(angulo_radianos)
+cosseno = math.cos(angulo_radianos)
+tangente = math.tan(angulo_radianos)
+
+print(f'Seno: {seno:.2f}')
+print(f'Cosseno: {cosseno:.2f}')
+print(f'Tangente: {tangente:.2f}')
--- exercicios/ex_19.py
+++ exercicios/ex_19.py
@@ -1,2 +1,15 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 # Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
+
+import random
+
+alunos = [
+    'Lucas Felipe',
+    'Mayara Rosa',
+    'Clara Matos',
+    'Marlon Grabriel',
+]
+
+aluno_sorteado = random.sample(alunos, 1)
+
+print(f'O aluno(a) sorteado foi {aluno_sorteado}'.join(aluno_sorteado))
