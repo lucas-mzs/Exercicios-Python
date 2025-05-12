254fc30d855513f0661e849b91f5900e6aae720c
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-12 00:50:10 -0300

    addicing exercises
    adicionando os exercícios 20 e 21.

--- exercicios/ex_20.py
+++ exercicios/ex_20.py
@@ -1,2 +1,18 @@
 # O mesmo professor do exercício anterior quer sortear a ordem de apresentação de trabalhos dos alunos. 
 # Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+import random
+
+alunos = [
+    'Lucas Felipe',
+    'Mayara Rosa',
+    'Clara Matos',
+    'Marlon Grabriel',
+]
+
+sorteio_alunos = random.sample(alunos, 4)
+
+print('O sortei dos alunos que iram apresentar o trabalho foi: ')
+
+for i, aluno in enumerate(sorteio_alunos):
+    print(f'{i})', aluno)
--- exercicios/ex_21.py
+++ exercicios/ex_21.py
@@ -1,1 +1,38 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+from pygame import mixer
+import time
+
+mixer.init()
+
+mixer.music.load('arquivo.mp3')
+
+mixer.music.play()
+
+print('\nComandos:')
+print('[p] - Pausar')
+print('[c] - Continuar')
+print('[s] - Parar')
+print('[q] - Sair do programa')
+
+while True:
+    comando = input("\nDigite um comando: ").lower()
+
+    if comando == 'p':
+        mixer.music.pause()
+        print('Música pausada.')
+
+    elif comando == 'c':
+        mixer.music.unpause()
+        print('Continuando música.')
+
+    elif comando == 's':
+        mixer.music.stop()
+        print('Música parada.')
+
+    elif comando == 'q':
+        print('Saindo...')
+        break
+
+    else:
+        print('Comando inválido. Use: p, c, s ou q.')
