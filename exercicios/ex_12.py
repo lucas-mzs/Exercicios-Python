0ba4f75e4463c055540e1c4932bc9c64b273497b
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-14 08:53:01 -0300

    Update ex_12.py

--- exercicios/ex_12.py
+++ exercicios/ex_12.py
@@ -1,6 +1,6 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
 preco = float(input('Informe o preço do produto: '))
-desconto = preco * 0.95
+desconto = preco * 0.05
 
 print(f'O seu produto com um desconto de 5% fica R${desconto}')
