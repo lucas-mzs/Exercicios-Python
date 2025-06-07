f36cb08b8bad7c27a798f2dbfb80aeba708e9336
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-07 02:10:05 -0300

    adding exercises
    adicionando os exercícios 30 e 31.

--- exercicios/ex_30.py
+++ exercicios/ex_30.py
@@ -1,1 +1,8 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+numero_usr = int(input('Digite um número: '))
+
+if numero_usr % 2 == 0:
+    print(f'{numero_usr} é par.')
+else:
+    print(f'{numero_usr} é ímpar.')
--- exercicios/ex_31.py
+++ exercicios/ex_31.py
@@ -1,2 +1,11 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em km. 
 # Calcule o preço da passagem, cobrando R$0,50 por km para viagens de até 200km e R$0,45 para viagens mais longas.
+
+distancia_viagem = float(input('Qual é a distancio da sua viagem: '))
+
+if distancia_viagem <= 200:
+    preco_pessagem_viagem_curta = distancia_viagem * 0.5
+    print(f'O valor do preço da passagem foi de R${preco_pessagem_viagem_curta:.2f}'.replace('.', ','))
+elif distancia_viagem > 200:
+    preco_pessagem_viagem_longa = distancia_viagem * 0.45
+    print(f'O valor do preço da passagem foi de R${preco_pessagem_viagem_longa:.2f}'.replace('.', ','))
