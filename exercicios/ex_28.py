b06b3e070227c27d5058393b34f12b9c849cc9a1
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-07 01:43:39 -0300

    adding exercises;
    adicionando os exercícios 28 e 29.

--- exercicios/ex_28.py
+++ exercicios/ex_28.py
@@ -1,3 +1,16 @@
-# Escreva um programa que faça o computador "pensar"em um número inteiro entre 0 e 5 e peça para 
+# Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para 
 # o usuário tentar descobrir qual foi o número escolhido pelo computador. 
 # O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+import random
+
+numero_cmp = random.randint(0, 5)
+
+numero_usr = int(input('Qual número entre 0 e 5 o computador escolheu: '))
+
+if numero_usr not in (0, 1, 2, 3, 4, 5):
+    print('Número inválido.')
+elif numero_usr == numero_cmp:
+    print('Você acertou!!')
+else:
+    print(f'Você errou! O número escolhido foi {numero_cmp}.')
--- exercicios/ex_29.py
+++ exercicios/ex_29.py
@@ -1,2 +1,11 @@
 # Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. 
 # A multa vai custar R$7,00 por cada km acima do limite.
+
+velocidade_carro = float(input('Digite a velocidade do seu carro: '))
+
+multa = velocidade_carro * 7
+
+if velocidade_carro >= 80:
+    print(f'Você foi multado no valor de {multa}')
+else:
+    print('Você está na velocidade correta. Boa viagem!')
