8f3c662b2902b9b83f83a598840a1bddabafc6e9
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-29 11:39:48 -0300

    adding a new exercise

--- exercicios/ex_32.py
+++ exercicios/ex_32.py
@@ -1,1 +1,8 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+
+def e_bissexto(ano):
+    return ano % 4 == 0 or ano % 100 != 0
+
+print(e_bissexto(2007))
+print(e_bissexto(2025))
+print(e_bissexto(2032))
