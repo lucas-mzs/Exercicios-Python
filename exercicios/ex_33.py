bb7a7f946bd5719ec744606678944bf978dc8692
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-30 00:11:44 -0300

    adding a new exercise

--- exercicios/ex_33.py
+++ exercicios/ex_33.py
@@ -1,1 +1,12 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+nmr_1 = 3
+nmr_2 = 2
+nmr_3 = 1
+
+if nmr_1 > nmr_2 and nmr_1 > nmr_3:
+    print(f'{nmr_1} é maior que os números {nmr_2} e {nmr_3}.')
+elif nmr_2 > nmr_1 and nmr_2 > nmr_3:
+    print(f'{nmr_2} é maior que os números {nmr_1} e {nmr_3}.')
+elif nmr_3 > nmr_1 and nmr_3 > nmr_2:
+    print(f'{nmr_3} é maior que os números {nmr_1} e {nmr_2}.')
