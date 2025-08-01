32db6a37a3f24e2bce40f883c6ccaa10e9b9da5b
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-01 08:38:58 -0300

    adding a new exercise

--- exercicios/ex_51.py
+++ exercicios/ex_51.py
@@ -1,1 +1,8 @@
 # Escreva um programa que exiba os múltiplos de 4 entre 1 e 100.
+
+def mostrar_multiplos():
+    for i in range(1, 101):
+        if i % 4 == 0:
+            print(i, end=' ')
+
+mostrar_multiplos()
