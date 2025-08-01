d65dee5ebafa4ceda8493bd1bd5346c0c3a89140
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-01 07:31:08 -0300

    adding a new exercise

--- exercicios/ex_46.py
+++ exercicios/ex_46.py
@@ -1,2 +1,15 @@
 # Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com 
 # uma pequena pausa de 1 segundo entre eles.
+
+import time
+
+def estourar_fogos():
+    print('=== Começar queima de fogos! ===')
+
+    for i in range(10, 0, -1):
+        print(f'\n{i}...')
+        time.sleep(1)
+
+    print('\n🎆 BUUM! EXPLODIU! 🎇')
+
+estourar_fogos()
