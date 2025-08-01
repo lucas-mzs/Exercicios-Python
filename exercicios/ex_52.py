ddb232e136194b3bf0c3edf480837c2471a8144c
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-01 08:48:21 -0300

    answering exercise 52

--- exercicios/ex_52.py
+++ exercicios/ex_52.py
@@ -1,1 +1,10 @@
 # Mostre os números ímpares entre 200 e 0 (ordem decrescente).
+
+def mostrar_impares():
+    print('=== Números ímpares entre 200 e 1 (decrescente) ===\n')
+
+    for i in range(199, 0, -2):
+        print(i, end=' ')
+    print('\n\n✅ Fim da lista.')
+
+mostrar_impares()
