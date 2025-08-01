400607da3472144e7cd78f48b88a925efd9f60eb
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-01 07:57:16 -0300

    adding a new exercise

--- exercicios/ex_49.py
+++ exercicios/ex_49.py
@@ -1,1 +1,7 @@
-# Refaça o exercício 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço FOR.
+# Refaça o exercício 09, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço FOR.
+
+num = int(input('Escolha um número: '))
+
+for i in range(10):
+    calculo = num * (i + 1)
+    print(f'{num} x {i + 1} = {calculo}')
