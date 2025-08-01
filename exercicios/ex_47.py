cea723ca7e1195579aa62039ea7f202857f43b5d
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-01 07:42:07 -0300

    adding a new exercise

--- exercicios/ex_47.py
+++ exercicios/ex_47.py
@@ -1,1 +1,9 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+def numeros_pares():
+    print('Números pares entre 1 e 50:')
+    for i in range(2, 51, 2):
+        print(i, end=', ')
+    print('\n\n✅ Fim da contagem.')
+
+numeros_pares()
