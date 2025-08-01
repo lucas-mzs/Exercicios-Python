712eb25f775f436c7a64ef46623bf4656bde106d
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-01 07:53:53 -0300

    adding a new exercise

--- exercicios/ex_48.py
+++ exercicios/ex_48.py
@@ -1,2 +1,16 @@
 # Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se 
 # encontram no intervalo de 1 até 500.
+
+def somando_impares():
+    soma = 0
+
+    print('=== Ímpares multiplos de 3 entre 1 e 500 ===')
+
+    for i in range(1, 501):
+        if i % 2 != 0 and i % 3 == 0:
+            soma += i
+            print(i, end=' ')
+
+    print(f'\n\n🔢 Soma total: {soma}')
+
+somando_impares()
