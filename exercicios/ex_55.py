a9c4cf3144d32f80d9b0053ddf512352134c929e
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-08 10:58:34 -0300

    answering exercise 55

--- exercicios/ex_55.py
+++ exercicios/ex_55.py
@@ -1,1 +1,18 @@
 # Peça 7 números ao usuário e calcule quantos são pares e quantos são ímpares.
+
+def contar_par_impar():
+    pares = 0
+    impares = 0
+
+    for i in range(1, 8):
+        numero = int(input(f'Digite o {i}º número inteiro: '))
+
+        if numero % 2 == 0:
+            pares += 1
+        else:
+            impares += 1
+    
+    print(f'\n📊 Quantidade de números pares: {pares}')
+    print(f'\n📊 Quantidade de números ímpares: {impares}')
+
+contar_par_impar()
