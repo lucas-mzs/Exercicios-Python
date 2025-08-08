57692b24346725d828f8b56196e77713b947e322
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-08 11:06:02 -0300

    answering exercise

--- exercicios/ex_56.py
+++ exercicios/ex_56.py
@@ -1,1 +1,7 @@
 # Imprima a tabuada de um número digitado pelo usuário.
+
+numero = int(input('Digite um número inteiro: '))
+
+for n in range(10):
+    tab = numero * (n + 1)
+    print(f'{numero} x {n + 1} = {tab}')
