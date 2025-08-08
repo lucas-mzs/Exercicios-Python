ab1b567adfb0133a3595329422c58bf927a17d9b
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-08 10:36:07 -0300

    answering exercise 54

--- exercicios/ex_54.py
+++ exercicios/ex_54.py
@@ -1,1 +1,8 @@
 # Some os números divisíveis por 5 entre 1 e 100.
+
+def numeros_divisiveis():
+    for i in range(101):
+        if i % 5 == 0:
+            print(i, end=' ')
+
+numeros_divisiveis()
