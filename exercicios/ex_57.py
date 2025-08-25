a7098939f55fd4b838e60f4ed90fdb665c28984a
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-25 14:22:30 -0300

    answering exercise

--- exercicios/ex_57.py
+++ exercicios/ex_57.py
@@ -1,1 +1,11 @@
 # Mostre a soma dos quadrados dos 20 primeiros números naturais.
+
+def soma_quadrados():
+    soma = 0
+
+    for n in range(1, 21):
+        soma += n ** 2
+    
+    print(f'A soma dos quadrados dos primeiros 20 números naturais é {soma}')
+
+soma_quadrados()
