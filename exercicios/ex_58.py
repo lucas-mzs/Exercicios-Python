bdf9eb4d398cf4344febca024039a0aba2ad11d7
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-08-25 14:29:01 -0300

    answering exercise 58

--- exercicios/ex_58.py
+++ exercicios/ex_58.py
@@ -1,1 +1,12 @@
 # Conte quantos múltiplos de 3 entre 1 e 500 também são pares.
+
+def qtd_multiplos():
+    contador = 0
+
+    for m in range(1, 501):
+        if m % 3 == 0 and m % 2 == 0:
+            contador += 1
+    
+    print(f'Quantidade de múltiplos de 3 que também são pares entre 1 e 500: {contador}')
+
+qtd_multiplos()
