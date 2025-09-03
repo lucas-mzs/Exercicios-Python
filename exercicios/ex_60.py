9238bf8358b4871b0d35ef6d7aadddb4c713d2fa
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-03 07:44:53 -0300

    answering exercise 60

--- exercicios/ex_60.py
+++ exercicios/ex_60.py
@@ -1,1 +1,17 @@
 # Solicite 5 números e informe o maior e o menor valor.
+
+def maior_menor():
+    numeros = []
+
+    for i in range(1, 6):
+        n = int(input(f'Digite o {i}º número: '))
+        numeros.append(n)
+    
+    maior = max(numeros)
+    menor = min(numeros)
+
+    print("\n=== RESULTADO ===")
+    print(f"📈 Maior valor digitado: {maior}")
+    print(f"📉 Menor valor digitado: {menor}")
+
+maior_menor()
