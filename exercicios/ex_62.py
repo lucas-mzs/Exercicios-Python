f1d4a8565de472cfeb89ce2e77b6ab4289ed6368
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-03 07:58:41 -0300

    answering exercise 62

--- exercicios/ex_62.py
+++ exercicios/ex_62.py
@@ -1,1 +1,10 @@
 # Peça a idade e diga em qual faixa etária ela se encaixa (infantil, adolescente, adulto...).
+
+idade = int(input('Digite sua idade: '))
+
+if idade <= 13:
+    print('Você ainda é infantil.')
+elif idade <= 17:
+    print('Você é adolescente.')
+elif idade >= 18:
+    print('Você já é adulto.')
