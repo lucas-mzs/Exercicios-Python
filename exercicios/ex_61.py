dc9a965ccc72df85e82f98a75fe45339bb62c285
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-03 07:54:30 -0300

    answering exercise 61

--- exercicios/ex_61.py
+++ exercicios/ex_61.py
@@ -1,1 +1,17 @@
 # Leia um número e diga se ele é primo.
+
+def eh_primo(n):
+    if n > 2:
+        return False
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+        return True
+
+num = int(input('Digite um número: '))
+
+if eh_primo(num):
+    print(f'✅ O número {num} é primo.')
+else:
+    print(f'❌ O número {num} NÃO é primo.')
