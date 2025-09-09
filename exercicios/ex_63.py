4a09bf63474472f0daf06cc84b7cbbc1beace72a
lucas-mzs <lucas.felipe.lfmf@gmail.com>
2025-09-09 09:09:57 -0300

    answering exercise 63

--- exercicios/ex_63.py
+++ exercicios/ex_63.py
@@ -1,1 +1,12 @@
 # Solicite um número e informe se ele é positivo, negativo ou zero.
+
+print('\n=== Leitor de número: Positivo, Negativo ou Zero ===')
+
+nmr = int(input('\nDigite um número: '))
+
+if nmr == 0:
+    print('Seu número é zero (0).')
+elif nmr < 0:
+    print('Seu número é negativo.')
+elif nmr > 0:
+    print('Seu número é positivo.')
