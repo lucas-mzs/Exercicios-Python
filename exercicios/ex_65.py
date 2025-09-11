385bb02d302cc10c252f8dc5c52d811a2511f206
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-11 08:06:53 -0300

    answering exercise 65

--- exercicios/ex_65.py
+++ exercicios/ex_65.py
@@ -1,1 +1,22 @@
 # Leia um número de 1 a 7 e mostre o dia da semana correspondente.
+
+def dias_semana():
+        dias = [
+                None,
+                'domingo',
+                'segunda-feira',
+                'terça-feira',
+                'quarta-feira',
+                'quinta-feira',
+                'sexta-feira',
+                'sábado'
+        ]
+
+        dia = int(input(f'Digite o número de 1 a 7 para saber o dia da semana: '))
+
+        if 1 <= dia <= 7:
+            print(f'O número {dia} corresponde ao dia {dias[dia]} da semana.')
+        else:
+            print('❌ Número inválido! Digite um número de 1 a 7.')
+        
+dias_semana()
