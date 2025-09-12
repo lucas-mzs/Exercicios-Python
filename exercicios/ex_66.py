4fed14621be4b266a79975a6e979c50d89e34fa7
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-12 07:49:01 -0300

    answering exercise 66

--- exercicios/ex_66.py
+++ exercicios/ex_66.py
@@ -1,1 +1,15 @@
 # Valide a entrada de um número entre 0 e 10. Repita até o usuário acertar.
+
+def validar_numero():
+    numero_correto = 7
+
+    while True:
+        numero = int(input('Digite um número entre 0 e 10: '))
+
+        if numero == numero_correto:
+            print(f'🎉 Parabéns! Você acertou, o número era {numero_correto}.')
+            break
+        else:
+            print('❌ Número inválido! Tente novamente.')
+
+validar_numero()
