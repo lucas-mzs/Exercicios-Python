3363f38d1d19873885b0041de48ec37571bd8060
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-15 14:52:51 -0300

    answering a exercise 68

--- exercicios/ex_68.py
+++ exercicios/ex_68.py
@@ -1,1 +1,8 @@
 # Peça um ano e diga se ele é bissexto.
+
+def eh_bissexto(ano):
+    return ano % 4 == 0 or ano % 100 != 0
+
+print(eh_bissexto(2025))
+print(eh_bissexto(2026))
+print(eh_bissexto(2032))
