e8c08c7561fb094a8b70764584d214aad55376b0
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-16 14:46:34 -0300

    answering exercise 70

--- exercicios/ex_70.py
+++ exercicios/ex_70.py
@@ -1,1 +1,17 @@
 # Leia dois números e exiba o maior e a diferença entre eles.
+
+def numero_maior_diferente():
+    n1 = float(input('Digite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+
+    if n1 > n2:
+        maior = n1
+        diferenca = n1 - n2
+    else:
+        maior = n2
+        diferenca = n2 - n1
+    
+    print(f'O maior número entre esses dois é: {maior}')
+    print(f'A diferença entre eles é de: {diferenca}')
+
+numero_maior_diferente()
