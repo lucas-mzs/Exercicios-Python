3e4b769bd16e6d07606738d0787df4e78e4d2150
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-16 14:55:00 -0300

    answering exercise 71

--- exercicios/ex_71.py
+++ exercicios/ex_71.py
@@ -1,1 +1,12 @@
 # Leia um número e mostre o seu fatorial.
+
+def fatorial_numero():
+    n = int(input('Digite um número inteiro para calcular o fatorial: '))
+    fatorial = 1
+
+    for i in range(1, n + 1):
+        fatorial *= i
+    
+    print(f'O fatorial de {n} é: {fatorial}')
+
+fatorial_numero()
