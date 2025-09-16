536692467938a110b3687744c0b84ff254977d60
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-16 14:41:26 -0300

    answering exercise 69

--- exercicios/ex_69.py
+++ exercicios/ex_69.py
@@ -1,1 +1,12 @@
 # Solicite um número de 4 dígitos e mostre invertido (ex: 1234 → 4321).
+
+def numeros_invertidos():
+    numeros = input('Digite quatro números: ')
+
+    if len(numeros) == 4 and numeros.isdigit():
+        invertido = numeros[::-1]
+        print(f'O número invertido é: {invertido}')
+    else:
+        print("Entrada inválida! Digite exatamente 4 dígitos.")
+
+numeros_invertidos()
