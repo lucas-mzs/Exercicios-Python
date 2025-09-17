db9bae6d6ec0cf3c36d56a8992086f5816677b89
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-17 07:02:42 -0300

    answering exercise 73

--- exercicios/ex_73.py
+++ exercicios/ex_73.py
@@ -1,1 +1,12 @@
 # Calcule os 10 primeiros termos de uma sequência de Fibonacci.
+
+def fibonacci_10():
+    termos = [0, 1]
+    while len(termos) < 10:
+        proximo = termos[-1] + termos[-2]
+        termos.append(proximo)
+    
+    print('Os 10 primeiros termos da sequência de Fibonacci são:')
+    print(termos)
+
+fibonacci_10()
