a240f6b2682c5c591bddc265e6461c7aa80717be
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-17 07:17:08 -0300

    answering exercise 74

--- exercicios/ex_74.py
+++ exercicios/ex_74.py
@@ -1,1 +1,20 @@
 # Peça 3 números e mostre a soma apenas dos pares.
+
+def soma_pares():
+    soma = 0
+    numeros = []
+
+    for i in range(1, 4):
+        n = int(input(f'Digite o {i}º número: '))
+        numeros.append(n)
+        if n % 2 == 0:
+            soma += n
+    
+    if soma > 0:
+        print(f'Você digitou os números: {numeros}')
+        print(f'A soma dos pares é {soma}')
+    else:
+        print(f'Você digitou os números: {numeros}')
+        print('Nenhum número par foi digitado.')
+
+soma_pares()
