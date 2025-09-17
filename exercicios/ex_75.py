f29f7ef8a178c52043e05b37a70f2d847986d739
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-17 07:21:18 -0300

    answering exercise 75

--- exercicios/ex_75.py
+++ exercicios/ex_75.py
@@ -1,1 +1,18 @@
 # Converta um número decimal para binário sem usar bin().
+
+def decimal_para_binario():
+    n = int(input('Digite um número decimal: '))
+    original = n
+    binario = ''
+
+    if n == 0:
+        binario = '0'
+    else:
+        while n > 0:
+            resto = n % 2
+            binario = str(resto) + binario
+            n = n // 2
+    
+    print(f'O número decimal {original} em binário é: {binario}')
+
+decimal_para_binario()
