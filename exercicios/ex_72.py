628c9c8348213f232e6132dc972c75a6923948b9
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-17 06:58:56 -0300

    answering exercise 72

--- exercicios/ex_72.py
+++ exercicios/ex_72.py
@@ -1,1 +1,12 @@
 # Mostre os 20 primeiros termos de uma PA (progressão aritmética).
+
+def pa_20_termos():
+    primeiro = int(input('Digite o primeiro termo da PA: '))
+    razao = int(input('Digite a razão da PA: '))
+
+    print('\nOs 20 primeiros termos da PA são:')
+    for i in range(20):
+        termo = primeiro + i * razao
+        print(termo, end=' ')
+
+pa_20_termos()
