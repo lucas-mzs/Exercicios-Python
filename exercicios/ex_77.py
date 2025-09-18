8e53473c7113bfa5e71f20772cfe923fb2791820
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-18 07:52:11 -0300

    answering exercise 77

--- exercicios/ex_77.py
+++ exercicios/ex_77.py
@@ -1,1 +1,16 @@
 # Peça um número e diga se é perfeito (soma dos divisores = número).
+
+def numero_perfeito():
+    n = int(input('Digite um número: '))
+    soma_divisores = 0
+
+    for i in range(1, n):
+        if n % i == 0:
+            soma_divisores += i
+    
+    if soma_divisores == n:
+        print(f'{n} é um número perfeito.')
+    else:
+        print(f'{n} não é um número perfeito.')
+
+numero_perfeito()
